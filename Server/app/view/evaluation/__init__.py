e29d1f03611a98188bdda932d8bab07bc7bd8f6d
miraedbswo <miraedbswo@gmail.com>
2019-02-10 04:14:02 +0900

    ADD: 여러번 설정 불가능하게 예외 처리

--- Server/app/view/evaluation/__init__.py
+++ Server/app/view/evaluation/__init__.py
@@ -1,9 +1,9 @@
 from flask import Blueprint
 from flask_restful import Api
 
-evaluation_blueprint = Blueprint('evaluation', __name__, url_prefix='/eval')
+evaluation_blueprint = Blueprint('evaluation', __name__)
 evaluation_api = Api(evaluation_blueprint)
 
 from .personality import PersonalityView
-evaluation_api.add_resource(PersonalityView, '/personality/{num}')
+evaluation_api.add_resource(PersonalityView, '/personality')
 
--- Server/app/view/evaluation/personality.py
+++ Server/app/view/evaluation/personality.py
@@ -7,30 +7,28 @@ from app.model.evaluation import PersonalityModel, AttentionModel, CharacterMode
 
 class PersonalityView(BaseResource):
     @jwt_required
-    def post(self, num):
+    def post(self):
+        num = request.json['num']
+        if PersonalityModel.get_personality(get_jwt_identity()):
+            return '', 409
+
         PersonalityModel.post_personality(get_jwt_identity(), num)
         return '', 201
 
-
-class AttentionView(BaseResource):
-    def get(self):
-        attention_list = AttentionModel.get_attention_list
-        return jsonify([attention for attention in attention_list]), 200
-
-
-class CharacterView(BaseResource):
-    def get(self):
-        character_list = CharacterModel.get_character_list
-        return jsonify([character for character in character_list]), 200
-
-
-class IntroductionView(BaseResource):
-    def get(self):
-        introduction_list = IntroductionModel.get_introduction_list
-        return jsonify([introduction for introduction in introduction_list]), 200
-
-    @jwt_required
-    def post(self, type):
-        PersonalityModel.post_personality(get_jwt_identity(), type)
-
-        return '', 201
+#
+# class AttentionView(BaseResource):
+#     def get(self):
+#         attention_list = AttentionModel.get_attention_list
+#         return jsonify([attention for attention in attention_list]), 200
+#
+#
+# class CharacterView(BaseResource):
+#     def get(self):
+#         character_list = CharacterModel.get_character_list
+#         return jsonify([character for character in character_list]), 200
+#
+#
+# class IntroductionView(BaseResource):
+#     def get(self):
+#         introduction_list = IntroductionModel.get_introduction_list
+#         return jsonify([introduction for introduction in introduction_list]), 200
