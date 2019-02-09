5a3bffd4d786e8a252d5717b9e327defd5a337a4
miraedbswo <miraedbswo@gmail.com>
2019-02-09 21:51:56 +0900

    FIX: Account url_prefix

--- Server/app/view/account/__init__.py
+++ Server/app/view/account/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask_restful import Api
 
-account_blueprint = Blueprint('account', __name__, url_prefix='/apply')
+account_blueprint = Blueprint('account', __name__, url_prefix='/account')
 account_api = Api(account_blueprint)
 
 from .auth import AuthView
