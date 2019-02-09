c958edd63159afed3e00595f9d23c628d567f60f
miraedbswo <miraedbswo@gmail.com>
2019-02-10 08:14:45 +0900

    ADD: mypage - id json

--- Server/app/view/account/mypage.py
+++ Server/app/view/account/mypage.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask_jwt_extended import jwt_required
 
 from app.view import BaseResource
@@ -6,5 +7,6 @@ from app.model import UserModel
 
 class MyPageView(BaseResource):
     @jwt_required
-    def get(self, id: str):
+    def get(self):
+        id = request.json['id']
         return UserModel.get_profile(id)
